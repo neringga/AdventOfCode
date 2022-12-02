e7330f2c74b9c6e6577ba2273023ac73beab8cf1
Neringa Geigalaite <geigalaite@gmail.com>
2022-12-02 12:37:55 +0200

    firstday-2

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,29 +1,29 @@
 import Foundation
 
-enum myErr: Error {
-    case runtimeError(String)
-}
-
 func countMaxCalories() throws -> Int? {
     let fileUrl = Bundle.main.url(forResource: "input", withExtension: "txt")
     let content = try String(contentsOf: fileUrl!, encoding: String.Encoding.utf8)
     let separatedContent: [String] = content.components(separatedBy: "\n")
     
-    var elvesKcal = Set<Int>()
+    var elvesKcal: [Int] = [0, 0, 0]
     var elfKcal = 0
         
     for item in separatedContent
     {
         guard let kcal = Int(item) else {
-            elvesKcal.insert(elfKcal)
+            if let lessKcalElfIndex = elvesKcal.firstIndex(where: { $0 < elfKcal}) {
+                elvesKcal[lessKcalElfIndex] = elfKcal
+                elvesKcal = elvesKcal.sorted()
+            }
+            
             elfKcal = 0
             continue
         }
         
         elfKcal = elfKcal + kcal
     }
-    
-    return elvesKcal.max()
+
+    return elvesKcal.reduce(0, +)
 }
 
 print(try countMaxCalories() ?? 0)
